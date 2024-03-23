60463dad783e461151c6602ced9ca3877dd37258
[Sahil khatiwada] <sahilkhatiwada12@gmail.com>
2024-03-24 00:01:07 +0545

    update navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-indigo-400">
             <strong className="text-blue-600">Sahil</strong>khatiwada
           </span>
